289d633ed0cc33697f8739c3a4c7d96cb7f47bc6
Jacob <jgulan@uncc.edu>
2020-01-22 19:19:56 +0000

    RubyBasics1 - Part1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,20 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  sum = 0
+  arr.each { |num|
+    sum = sum + num
+  }
+  return sum
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
