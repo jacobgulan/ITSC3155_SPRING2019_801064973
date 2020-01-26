742588e7172bffcef242a4c95409cec34b36fad1
Jacob <jgulan@uncc.edu>
2020-01-26 21:56:05 +0000

    RubyHashes - Part 2

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -19,9 +19,24 @@ end
 # Part II
 def array2d_2_hash contact_info, contacts
     # YOUR CODE HERE
+    if (contact_info[0] == '' || contact_info == nil || contact_info == [[]])
+        return contacts
+    end
+    
+    h = contacts
+    i = 0
+    contacts.each do |key, value|
+      h[key][:email] = contact_info[i][0]
+      h[key][:phone] = contact_info[i][1]
+      
+      i += 1
+    end
+    return h
+    
 end
 
 # Part III
 def hash_2_array contacts
     # YOUR CODE HERE
+    
 end
