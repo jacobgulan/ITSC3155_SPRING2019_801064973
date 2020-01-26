a0dbc63f4b7461d0ec7a891bb81f7fd51dba6b29
Jacob <jgulan@uncc.edu>
2020-01-26 21:17:30 +0000

    RubyHashes - Part 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -2,6 +2,18 @@
 # Part I
 def array_2_hash emails, contacts
     # YOUR CODE HERE
+    if (emails.length == 0)
+        return contacts
+    end
+    
+    h = {}
+    i = 0
+    contacts.each do |key, value|
+      h[key] = emails[i]
+      i += 1
+    end
+    return h
+  
 end
 
 # Part II
